7162f3ba33f8febfe947bffd4b97516feea1fe31
Martin Gouverneur <martingouv@MacBook-Air-de-Martin.local>
2023-01-09 20:03:32 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    
+    return render_template("templates/index.html")
---
+++ src/data.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+COLORS = {
+	fire: '#FDDFDF',
+	grass: '#DEFDE0',
+	electric: '#FCF7DE',
+	water: '#DEF3FD',
+	ground: '#f4e7da',
+	rock: '#d5d5d4',
+	fairy: '#fceaff',
+	poison: '#98d7a5',
+	bug: '#f8d5a3',
+	dragon: '#97b3e6',
+	psychic: '#eaeda1',
+	flying: '#F5F5F5',
+	fighting: '#E6E0D4',
+	normal: '#F5F5F5'
+}
+
+
+def get_pokemon(id: int) -> dict:
+    req = requests.get(f"https://pokeapi.co/api/v2/pokemon/{id}/")
+    data = json.loads(req.content)
+    name = data['name']
+    type = data["types"][0]["type"]["name"]
+    color = COLORS[type]
+    picture = ""
+    data = {
+        'name': name,
+        'type': type,
+        'color': color,
+    }
+    return data
+
+get_pokemon(1)
