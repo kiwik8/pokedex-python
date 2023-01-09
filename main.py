f1565515fdec649fea13ce9f6d5b2a709d711209
Martin Gouverneur <martingouv@MacBook-Air-de-Martin.local>
2023-01-09 21:27:41 +0100

    Webapp basic functionnalities

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from flask import Flask
 from flask import render_template
+from src.data import get_pokemon
+from time import sleep
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="src/templates", static_folder="src/static")
 
 @app.route("/")
-def main():
-    
-    return render_template("templates/index.html")
+@app.route("/<int:limit>")
+def main(limit=50):
+    html = []
+    for i in range(1, limit+1):
+        data = get_pokemon(i)
+        html.append(data)
+    return render_template("index.html", items=html)
+
+
+if __name__ == "__main__":
+    app.run()
--- src/data.py
+++ src/data.py
@@ -2,21 +2,23 @@ import requests
 import json
 
 COLORS = {
-	fire: '#FDDFDF',
-	grass: '#DEFDE0',
-	electric: '#FCF7DE',
-	water: '#DEF3FD',
-	ground: '#f4e7da',
-	rock: '#d5d5d4',
-	fairy: '#fceaff',
-	poison: '#98d7a5',
-	bug: '#f8d5a3',
-	dragon: '#97b3e6',
-	psychic: '#eaeda1',
-	flying: '#F5F5F5',
-	fighting: '#E6E0D4',
-	normal: '#F5F5F5'
+	"fire": '#FDDFDF',
+	"grass": '#DEFDE0',
+	"electric": '#FCF7DE',
+	"water": '#DEF3FD',
+	"ground": '#f4e7da',
+	"rock": '#d5d5d4',
+	"fairy": '#fceaff',
+	"poison": '#98d7a5',
+	"bug": '#f8d5a3',
+	"dragon": '#97b3e6',
+	"psychic": '#eaeda1',
+	"flying": '#F5F5F5',
+	"fighting": '#E6E0D4',
+	"normal": '#F5F5F5'
 }
+
+
 
 
 def get_pokemon(id: int) -> dict:
@@ -25,12 +27,14 @@ def get_pokemon(id: int) -> dict:
     name = data['name']
     type = data["types"][0]["type"]["name"]
     color = COLORS[type]
-    picture = ""
+    picture = data["sprites"]["front_default"]
+    element_style = f"background-color: {color}"
+    formatted_id = str(id).zfill(3)
     data = {
         'name': name,
         'type': type,
-        'color': color,
+        'element_style': element_style,
+        'picture': picture,
+        'formatted_id': formatted_id,
     }
     return data
-
-get_pokemon(1)
